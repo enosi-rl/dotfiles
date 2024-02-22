918fd1e6c10be4594c463ca796b5a38ce7227d36
Robert Leftwich <robert@enosi.energy>
2024-02-23 08:51:54 +1100

    Different colour scheme

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -10,9 +10,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
+-- color scheme --
 -- config.color_scheme = 'Gruvbox light, hard (base16)'
 -- config.color_scheme = 'Gruvbox light, medium (base16)'
 -- config.color_scheme = 'Gruvbox light, soft (base16)'
@@ -20,8 +18,10 @@ end
 -- config.color_scheme = 'Gruvbox dark, pale (base16)'
 -- config.color_scheme = 'Gruvbox Material (Gogh)'
 
--- config.color_scheme = 'MonokaiPro (Gogh)'
-config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
+config.color_scheme = 'MonokaiPro (Gogh)'
+-- config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
+-- config.color_scheme = 'Monokai (terminal.sexy)'
+-- config.color_scheme = 'Mocha (dark) (terminal.sexy)'
 
 
 config.font = wezterm.font 'MonoLisa'
