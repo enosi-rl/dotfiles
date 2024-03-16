dbd2e5d7d86d89f3b44085054feaf18ba0a0e93e
Robert Leftwich <robert@enosi.energy>
2024-03-16 14:06:24 +1100

    Switched back to gruvbox. Formatting

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,33 +7,32 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- color scheme --
 -- config.color_scheme = 'Gruvbox light, hard (base16)'
 -- config.color_scheme = 'Gruvbox light, medium (base16)'
 -- config.color_scheme = 'Gruvbox light, soft (base16)'
--- config.color_scheme = 'Gruvbox dark, medium (base16)'
+config.color_scheme = "Gruvbox dark, medium (base16)"
 -- config.color_scheme = 'Gruvbox dark, pale (base16)'
 -- config.color_scheme = 'Gruvbox Material (Gogh)'
 
-config.color_scheme = 'MonokaiPro (Gogh)'
+-- config.color_scheme = 'MonokaiPro (Gogh)'
 -- config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
 -- config.color_scheme = 'Monokai (terminal.sexy)'
 -- config.color_scheme = 'Mocha (dark) (terminal.sexy)'
 
-
-config.font = wezterm.font 'MonoLisa'
-config.audible_bell = 'Disabled'
+config.font = wezterm.font("MonoLisa")
+config.audible_bell = "Disabled"
 config.visual_bell = {
-  fade_in_function = 'EaseIn',
-  fade_in_duration_ms = 150,
-  fade_out_function = 'EaseOut',
-  fade_out_duration_ms = 150,
+	fade_in_function = "EaseIn",
+	fade_in_duration_ms = 150,
+	fade_out_function = "EaseOut",
+	fade_out_duration_ms = 150,
 }
 config.colors = {
-  visual_bell = '#303030',
+	visual_bell = "#303030",
 }
 
 config.window_decorations = "RESIZE"
@@ -43,7 +42,7 @@ config.initial_cols = 121
 config.show_update_window = true
 
 config.keys = {
-  { key = 'A', mods = 'CMD', action = wezterm.action.ShowTabNavigator },
+	{ key = "A", mods = "CMD", action = wezterm.action.ShowTabNavigator },
 }
 
 -- and finally, return the configuration to wezterm
